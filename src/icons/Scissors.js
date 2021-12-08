cb46df45fba4f8d14f6170872fc3b1cc050d21a9
Konrad Kukla <kondziocard@gmail.com>
2021-12-09 00:49:02 +0100

    add logic

--- src/icons/Scissors.js
+++ src/icons/Scissors.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-export default function() {
+function Scissors() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,3 +14,5 @@ export default function() {
     </svg>
   );
 }
+
+export default Scissors;
